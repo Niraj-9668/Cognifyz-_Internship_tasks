4349ddc43b2a3320f585159190504763b7820c16
Niraj Kumar Sahoo <sahoonirajkumar2001@gmail.com>
2024-09-02 12:10:54 +0530

    Add files via upload

---
+++ Level1/Task2/part1/button.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('Button');
+button.addEventListener('click', function () {
+    button.style.backgroundColor = 'red';
+    button.style.borderColor = 'green';
+    button.style.color = 'black';
+    button.style.boxShadow = '0 0 50px yellow';
+});
